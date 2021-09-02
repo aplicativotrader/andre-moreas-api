b45c40220784410173a830e9be47e1e93117ceb6
Jonatas Souza <jonatassouza@192.168.1.4>
2021-09-02 08:05:01 -0300

    ajuste timezone

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb://andremoraes01:Andre01*01@mongo71-farm10.kinghost.net/andremoraes01";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+process.env.TZ = 'America/Sao_Paulo' 
 module.exports = client;
